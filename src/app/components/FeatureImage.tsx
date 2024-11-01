3f3cfd6e45c339f48c33b16b4a8f6db978896c23
DicksonPreye <woyengicaleb@gmail.com>
2024-11-01 10:07:57 -0400

    Initial commit

--- src/app/components/FeatureImage.tsx
+++ src/app/components/FeatureImage.tsx
@@ -92,7 +92,7 @@ export default function FeatureImage() {
       {isModalOpen && (
         <div className="overlay" onClick={closeModal}>
           <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <img src={selectedImage} alt="Selected Image" />
+            <img src={selectedImage as string} alt="Selected Image" />
             <button onClick={closeModal} className="close-modal">
             <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path 
             d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 
